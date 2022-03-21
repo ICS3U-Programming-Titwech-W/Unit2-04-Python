e422ab728fd1b7c10a9ffa4bd3c634f9954c83f8
Ubuntu <ubuntu@ip-172-31-78-88.ec2.internal>
2022-03-21 00:02:18 +0000

    this progam asks the user for the diameter of a pizza then tells the user the price with tax.

---
+++ constants.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+# Created by: Titwech
+# Created on: March 11. 2022
+# This program contains constants.
+HST = 0.13
+LABOUR_COST = 2.00
+RENTAL_COST = 2.25
+INGRED_COST = 1.5
+
